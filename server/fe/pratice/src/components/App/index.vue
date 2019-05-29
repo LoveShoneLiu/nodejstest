ac6127aa93fe985871511bf32db20ee50bb30dc8
MOMO <liu.shaofei@immomo.com>
2019-05-29 17:53:56 +0800

    添加测试目录

---
+++ server/fe/pratice/src/components/App/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div style="width: 100%;height: 100%;background: #f6f6f6;">
+        test
+    </div>
+</template>
+<script>
+    import axios from 'axios';
+    import qs from 'qs';
+    export default ({
+        data() {
+            return {
+                
+            }
+        }
+    })
+</script>
+<style scoped>
+</style>
